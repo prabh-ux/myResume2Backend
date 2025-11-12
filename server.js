fbba82644e7a31f7f9da723b4d3645a1662ebce5
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-11-12 15:39:04 +0530

    added url in cors

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ import newsLetterRoute from './routes/newsLetterRoutes.js';
 const app=express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:[
+         "http://localhost:5173",
+         "https://prabhkiratsinghresume.vercel.app"   
+    ]
+}));
 
 app.use('/newsletter',newsLetterRoute);
 
